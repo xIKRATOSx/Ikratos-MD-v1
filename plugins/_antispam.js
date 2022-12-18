847ade28f368a9c46e11de7aa9e1ef8701d5ad35
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2022-12-18 19:21:47 +0500

    Ikratos-MD-v1

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -10,7 +10,7 @@ export async function all(m) {
             if (this.spam[m.sender].count > 10) {
                global.db.data.users[m.sender].banned = true
                 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
-    let caption = ` Banned *@${who.split("@")[0]}* Jangan spam kak!`
+    let caption = ` Banned *@${who.split("@")[0]}* Don't spam sis!`
     this.sendButton(m.chat, caption, wm, null, [['Matikan AntiSpam', '/disable antispam']], m, { mentions: this.parseMention(caption) })
             }
             this.spam[m.sender].count = 0
