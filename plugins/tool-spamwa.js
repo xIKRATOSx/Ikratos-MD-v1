7f4b20eaa18e510ae7d1a0eee3010a9e003b271c
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2022-12-18 18:33:04 +0500

    Ilratos-MD-v1

--- plugins/tool-spamwa.js
+++ plugins/tool-spamwa.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, text }) => {
 
 let [nomor, pesan, jumlah] = text.split('|')
-if (!nomor) throw '*[ ⚠️ ] HARAP MASUKKAN NOMOR YANG AKAN DI SPAM PESAN!*\n*PENGGUNAAN YANG BENAR:*\n*—◉ #spamwa nomor|teks|jumlah*\n*jumlah:*\n*—◉ #spamwa 5219999999999|hai :v|25*'
-if (!pesan) throw '*[ ⚠️ ] HARAP MASUKKAN PESAN KE SPAM!*\n*GUNAKAN YANG BENAR:*\n*—◉ #spamwa nomor|teks|jumlah*\n*CONTOH:*\n*—◉ #spamwa 5219999999999|merespons :v|25*'
-if (jumlah && isNaN(jumlah)) throw '*[ ⚠️ ] KUANTITAS HARUS ADALAH NOMOR!*\n*PENGGUNAAN YANG TEPAT:*\n*—◉ #spamwa nomor|teks|jumlah*\n*CONTOH:*\n*—◉ #spamwa 5219999999999|merespons :v|25*'
+if (!nomor) throw '*[ ⚠️ ] PLEASE ENTER THE NUMBER THAT WILL BE SPAMED IN THE MESSAGE!*\n*CORRECT USE:*\n*—◉ #spamwa number|text|amount*\n*amount:*\n*—◉ #spamwa 5219999999999|hai :v|25*'
+if (!pesan) throw '*[ ⚠️ ] PLEASE PUT A MESSAGE TO SPAM!*\n*USE CORRECT:*\n*—◉ #spamwa number|text|amount*\n*XAMPLE:*\n*—◉ #spamwa 5219999999999|merespons :v|25*'
+if (jumlah && isNaN(jumlah)) throw '*[ ⚠️ ] QUANTITY MUST BE A NUMBER!*\n*PROPER USE:*\n*—◉ #spamwa umber|text|amount*\n*EXAMPLE:*\n*—◉ #spamwa 5219999999999|merespons :v|25*'
 
 let fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net'
 let fixedJumlah = jumlah ? jumlah * 1 : 10
-if (fixedJumlah > 50) throw '*[ ⚠️ ] TERLALU BANYAK PESAN! JUMLAH HARUS KURANG DARI 50 PESAN*️'
-await m.reply(`*[❗] SPAM PESAN KE NOMOR ${nomor} ITU SUKSES DILAKUKAN*\n*JUMLAH TERKIRIM:*\n*—◉ ${fixedJumlah} waktu!*`)
+if (fixedJumlah > 50) throw '*[ ⚠️ ] TOO MANY MESSAGES! QUANTITY MUST BE LESS THAN 50 MESSAGES*️'
+await m.reply(`*[❗] SPAM MESSAGE TO NUMBER ${number} WAS SUCCESSFULLY DONE*\n*NUMBER SENT:*\n*—◉ ${fixedJumlah} time(s)!*`)
 for (let i = fixedJumlah; i > 1; i--) {
 if (i !== 0) conn.reply(fixedNumber, pesan.trim(), m)
 }}
