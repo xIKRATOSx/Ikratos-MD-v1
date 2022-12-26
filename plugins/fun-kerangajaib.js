de3605856ea325432a7f5d5f6e6155b32d093ea4
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2022-12-26 17:31:15 +0500

    0000

--- plugins/fun-kerangajaib.js
+++ plugins/fun-kerangajaib.js
@@ -1,17 +1,17 @@
 let handler = async (m, { text, command, usedPrefix }) => {
     if (!text) throw `Use example ${usedPrefix}${command} i'm alien?`
     m.reply(`"${[
-        'Mungkin suatu hari',
-        'Tidak juga',
-        'Tidak keduanya',
-        'Kurasa tidak',
-        'Ya',
-        'Coba tanya lagi',
-        'Tidak ada'
+        'Maybe one day',
+        'Not really',
+        'Not both',
+        'I think not',
+        'Yes',
+        'Try asking again',
+        'There is none'
     ].getRandom()}."`)
 }
-handler.help = ['kerang', 'kerangajaib'].map(v => v + ' <teks>')
-handler.tags = ['kerang', 'fun']
+handler.help = ['clams', 'kerangajaib'].map(v => v + ' <teks>')
+handler.tags = ['clams', 'fun']
 
 handler.command = /^(kulit)?kerang(ajaib)?$/i
 
