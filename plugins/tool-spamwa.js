f609c13d5b166a8ceb827e2a78649f5e2575b90f
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-01-18 22:10:12 +0500

    Update tool-spamwa.js

--- plugins/tool-spamwa.js
+++ plugins/tool-spamwa.js
@@ -8,7 +8,7 @@ if (jumlah && isNaN(jumlah)) throw '*[ ⚠️ ] QUANTITY MUST BE A NUMBER!*\n*PR
 let fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net'
 let fixedJumlah = jumlah ? jumlah * 1 : 10
 if (fixedJumlah > 50) throw '*[ ⚠️ ] TOO MANY MESSAGES! QUANTITY MUST BE LESS THAN _50_ MESSAGES*️'
-await m.reply(`*[❗] SPAM MESSAGE TO NUMBER ${number} WAS SUCCESSFULLY DONE*\n*NUMBER SENT:*\n*—◉ ${fixedJumlah} time(s)!*`)
+await m.reply(`*[❗] SPAM MESSAGE TO NUMBER WAS SUCCESSFULLY DONE*\n*NUMBER SENT:*\n*—◉ ${fixedJumlah} time(s)!*`)
 for (let i = fixedJumlah; i > 1; i--) {
 if (i !== 0) conn.reply(fixedNumber, pesan.trim(), m)
 }}
