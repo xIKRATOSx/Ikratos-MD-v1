49132654f60f0298e834f9e7b760a744c2fc5088
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-01-19 18:34:09 +0500

    Update tool-spamwa.js

--- plugins/tool-spamwa.js
+++ plugins/tool-spamwa.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn, text }) => {
 
-let [nomor, pesan, jumlah] = text.split('|')
-if (!nomor) throw '*[ ⚠️ ] PLEASE ENTER THE NUMBER THAT WILL BE SPAMED IN THE MESSAGE!*\n*CORRECT USE:*\n*—◉ #spamwa number|text|amount*\n*amount:*\n*—◉ #spamwa 5219999999999|hai :v|25*'
+let [number, pesan, jumlah] = text.split('|')
+if (!number) throw '*[ ⚠️ ] PLEASE ENTER THE NUMBER THAT WILL BE SPAMED IN THE MESSAGE!*\n*CORRECT USE:*\n*—◉ #spamwa number|text|amount*\n*amount:*\n*—◉ #spamwa 5219999999999|hai :v|25*'
 if (!pesan) throw '*[ ⚠️ ] PLEASE PUT A MESSAGE TO SPAM!*\n*USE CORRECT:*\n*—◉ #spamwa number|text|amount*\n*XAMPLE:*\n*—◉ #spamwa 5219999999999|merespons :v|25*'
 if (jumlah && isNaN(jumlah)) throw '*[ ⚠️ ] QUANTITY MUST BE A NUMBER!*\n*PROPER USE:*\n*—◉ #spamwa umber|text|amount*\n*EXAMPLE:*\n*—◉ #spamwa 5219999999999|merespons :v|25*'
 
-let fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net'
+let fixedNumber = number.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net'
 let fixedJumlah = jumlah ? jumlah * 1 : 10
 if (fixedJumlah > 50) throw '*[ ⚠️ ] TOO MANY MESSAGES! QUANTITY MUST BE LESS THAN _50_ MESSAGES*️'
-await m.reply(`*[❗] SPAM MESSAGE TO NUMBER WAS SUCCESSFULLY DONE*\n*NUMBER SENT:*\n*—◉ ${fixedJumlah} time(s)!*`)
+await m.reply(`*[❗] SPAM MESSAGE TO NUMBER ${number} WAS SUCCESSFULLY DONE*\n*NUMBER SENT:*\n*—◉ ${fixedJumlah} time(s)!*`)
 for (let i = fixedJumlah; i > 1; i--) {
 if (i !== 0) conn.reply(fixedNumber, pesan.trim(), m)
 }}
