5c062163b55d449b16c1be205e2d110e46de25f1
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-03-25 15:47:18 +0500

    Ikratos-MD-v1
    Update downloader-gitclone.js

--- plugins/downloader-gitclone.js
+++ plugins/downloader-gitclone.js
@@ -1,15 +1,18 @@
-import fetch from 'node-fetch'
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
-let handler = async (m, {conn, text, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Example user ${usedPrefix}${command} xIKRATOSx|Ikratos-MD-v1`
-    let [usr, rep] = text.split`/`
-    let url = `https://api.github.com/repos/${encodeURIComponent(usr)}/${encodeURIComponent(rep)}/zipball`
-    let name = `${encodeURIComponent(rep)}.zip`
-    m.reply(`L o a d i n g. . .`)
-    conn.sendFile(m.chat, url, name, null, m)
+var handler = async (m, { args, usedPrefix, command }) => {
+    if (!args[0]) throw `Example use ${usedPrefix}${command} xIKRATOSx|Ikratos-MD-v1`
+    if (!regex.test(args[0])) throw 'Url Is Not Valid! '
+    let [_, user, repo] = args[0].match(regex) || []
+    repo = repo.replace(/.git$/, '')
+    let url = `https://api.github.com/repos/${user}/${repo}/zipball`
+    let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
+    m.reply(`D o w n l o a d i n g. . .`)
+    conn.sendFile(m.chat, url, filename, null, m)
 }
-handler.help = ['gitclone <username>/<repo>']
+handler.help = ['gitclone'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /gitclone/i
+
+handler.limit = true
 
 export default handler
