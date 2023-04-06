27e3dea448782f9057c24bd47e963aa20dbdefe8
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-04-06 11:29:08 +0500

    Update

--- plugins/youtube-yta.js
+++ plugins/youtube-yta.js
@@ -4,13 +4,23 @@ let handler = async (m, { conn, args }) => {
 if (!args[0]) throw '*ENTRE THE LINK OF A YOUTUBE VIDEO*'
 await m.reply(`*Wait Downloading Your Audio 🔄*`)
 try {
+    if (!args[0]) throw 'ENTRE THE LINK OF A YOUTUBE VIDEO'
 let q = '128kbps'
 let v = args[0]
-const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v))
+
+// Kocak
+const yt = await youtubedl(v).catch(async () => await  youtubedlv2(v))
 const dl_url = await yt.audio[q].download()
 const ttl = await yt.title
 const size = await yt.audio[q].fileSizeH
-await conn.sendFile(m.chat, dl_url, ttl + '.mp3', null, m, false, { mimetype: 'audio/mp4' })
+
+await m.reply(`
+▢ Tɪᴛᴛʟᴇ: ${ttl}
+▢  Sɪᴢᴇ: ${size}
+
+▢ Ｌｏａｄｉｎｇ. . .`)
+await conn.sendMessage(m.chat, { document: { url: dl_url}, mimetype: 'audio/mpeg', fileName: `${ttl}.mp3`}, {quoted: m})
+// If u want a not document you can change document to audio, mimetype 'audio/mp4' n remove .mp3 on fileName
 } catch {
 try {
 let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=85faf717d0545d14074659ad&url=${args[0]}`)    
@@ -19,7 +29,15 @@ let n = lolh.result.title || 'error'
 await m.reply(`*Wait Uploading Your Audio 🔃*`)
 await conn.sendFile(m.chat, lolh.result.link, `${n}.mp3`, null, m, false, { mimetype: 'audio/mp4' })    
 } catch {
+ try {
+let q = '128kbps'
+const dl_url = await yt.audio[q].download()
+const ttl = await yt.title
+const size = await yt.audio[q].fileSizeH
+await conn.sendFile(m.chat, dl_url, ttl + '.mp3', null, m, false, { mimetype: 'audio/mp4' })
+} catch {
 await conn.reply(m.chat, '*404 ERROR UPLOADING SERVERS ARE BUSY*\n*TRY LATER...*', m)}
 }}
+}
 handler.command = /^y((outube|tb)audio|(outube|tb?)mp3|utubaudio|taudio|ta)$/i
 export default handler
