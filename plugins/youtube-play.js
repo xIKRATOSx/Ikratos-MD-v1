1d968e49d766b2b94c0c0eedd508edd652fcb434
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-04-12 13:15:36 +0500

    v1.0.0

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -14,7 +14,7 @@ try {
 📔 *Description:* ${description}
 ⏲️ *Published:* ${publishedTime}
 ⌚ *Duration:* ${durationH}
-👁️ *Views:* ${viewH}
+👀 *Views:* ${viewH}
   `
   await conn.sendButton(m.chat, whmodsdev, wm, botdate, [
     ['🎶 Audio', `${usedPrefix}yta ${url} yes`],
@@ -34,38 +34,12 @@ if (!text) throw `Use example ${usedPrefix}${command} arcade`
 📔 *Description:* ${description}
 ⏲️ *Published:* ${publishedTime}
 ⌚ *Duration:* ${durationH}
-👁️ *Views:* ${viewH}
+👀 *Views:* ${viewH}
   `
   let buttons = [{ buttonText: { displayText: '🎶 Audio' }, buttonId: `${usedPrefix}opta ${url}` }, { buttonText: { displayText: '🎥 Video' }, buttonId: `${usedPrefix}optv ${url}` }]
   let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: capt, footer: 'Follow me on instagram:\n\tinstagram.com/itx_ahmad.ali', buttons }, { quoted: m })
   // if (durationS > 4000) return conn.sendMessage(m.chat, { text: `*Download:* ${await shortUrl(ytLink)}\n\n_Duration too long..._` }, { quoted: msg })
 } 
-  /*catch {
- /**
- *
- * @param {*} query
- * @param {*} options
- * @returns
- *
-async function search(query, options = {}) {
-  const search = await yts.search({ query, hl: "es", gl: "ES", ...options });
-  return search.videos;
-}
-
-if (!text) throw `Use example ${usedPrefix}${command} arcade`
-const yt_play = await search(args.join(" "));
-let text1 = `*${htki} PLAY ${htka}*
-  📌 *Title:* ${yt_play[0].title}
-🔗 *Url:* ${yt_play[0].url}
-⏲️ *Published:* ${yt_play[0].ago}
-⌚ *Duration:* ${secondString(yt_play[0].duration.seconds)}
-👁️ *Views:* ${`${MilesNumber(yt_play[0].views)}`}
-  `.trim();
-let buttons = [{ buttonId: `${usedPrefix}opta ${url}`, buttonText: { displayText: '🎶 Audio' }, type: 1 }, { buttonId: `${usedPrefix}optv ${url}`, buttonText: { displayText: '🎥 Video' }, type: 1 }]
-let msg = await conn.sendMessage(m.chat, { image: { url: yt_play[0].image }, caption: text1, footer: 'Follow me on instagram:\n\tinstagram.com/itx_ahmad.ali', buttons }, { quoted: m })
-} catch {
-  throw `I\'m Getting API Update Right Now... Please Try Later...`
- }*/
 }
 handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader', 'limitmenu']
