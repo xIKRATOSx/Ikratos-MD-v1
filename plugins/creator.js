269b6283dd6f5a76ddc74a2887d7ef3d04c22d9f
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-04-12 17:01:22 +0500

    v1.0.0

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,23 +1,18 @@
-// Source https://github.com/sadteams/bot-md
-const delay = time => new Promise(res => setTimeout(res, time))
-let handler = async(m, { conn }) => {
-	conn.p = conn.p ? conn.p : {}
-	let id = m.chat
-	conn.p[id] = [
-	await conn.sendKontak(m.chat, global.owner, m, { contextInfo: { externalAdReply :{
-    showAdAttribution: true,
-     }}
-  })
-	]
-	await delay(100)
-  return conn.sendMessage(m.chat, { text: `Listen baby @${m.sender.split('@')[0]}, That's My Owner Don't be over, Okay? 😃\nTry to be polite\nDon\t call him or you will be ignored`, mentions: [m.sender] }, { quoted: conn.p[id][0]
-  })
-  await delay(100)
-  return delete conn.p[id]
-}
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let name = await conn.getName(who)
 
-handler.help = ['owner']
+  const sentMsg = await conn.sendContactArray(m.chat, [
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 Developer Bot `, `ɪɴɴᴏᴄᴇɴᴛ ᴍᴏʀᴇ ᴛʜᴀɴ ɢᴏᴏɢʟᴇ`, `ikratosofc@gmail.com`, `🇵🇰 Pakistan`, `🐙 https://github.com/xIKRATOSx`, `👑 ᴏᴡɴᴇʀ ɪᴋʀᴀᴛᴏs ʙᴏᴛ`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ`, `📵 ᴅᴏɴᴛ sᴘᴀᴍ/ᴄᴀʟʟ ᴍᴇ 😢`, `ɴᴏᴛʜɪɴɢ`, `🇵🇰 Pakistan`, `🐙 https://github.com/xIKRATOSx/Ikratos-MD-v1`, `🤖 ɪᴋʀᴀᴛᴏs-ᴍᴅ-ᴠ𝟷`]
+  ], fkontak)
+  await m.reply(`Listen baby @${m.sender.split('@')[0]}, That's My Owner Don't be over, Okay? 😃\nTry to be polite\nDon\t call him or you will be ignored`)
+  } 
+handler.help = ['owner', 'creator']
 handler.tags = ['info']
+
 handler.command = /^(owner|creator)$/i
 
 export default handler
