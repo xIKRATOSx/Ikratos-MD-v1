1b87d58a5c0a92da832bb83c013efe4be9177ae5
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-04-20 05:06:34 -0700

    v1.0.0

--- README.md
+++ README.md
@@ -51,12 +51,12 @@
 ## ```STEPS TO DEPLOY ON HEROKU```
 
 - 1. Fork this repo (Don't forget to give a star)
-- 2. ***Get [SESSION-ID](https://scanner.xikratosx.repl.co) by scanning QR code. `Whatapp>Three dots>Linked Devices`***
+- 2. ***Get [SESSION-ID](https://replit.com/@xIKRATOSx/scanner) by scanning QR code. `Whatapp>Three dots>Linked Devices`***
 - 3. Upload your session file in "session" folder
 - 4. Simply click Deploy to heroku button above
 
 ## ```QR SCAN```
-[![Run on Repl.it](https://repl.it/badge/github/xIKRATOSx/Ikratos-MD-v1)](https://scanner.xikratosx.repl.co)
+[![Run on Repl.it](https://repl.it/badge/github/xIKRATOSx/Ikratos-MD-v1)](https://replit.com/@xIKRATOSx/scanner)
 <img src="https://fontmeme.com/permalink/220116/0c42dc0b64931810388ba399da55e927.png" alt="backwoods-logo-font" border="0"></a>  
 ---------
 
--- plugins/_alive.js
+++ plugins/_alive.js
@@ -1,30 +1,30 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
   let caption = `
-  ╭──────[Ikratos-MD-v1]──────╮
-  │╭──────────────────────────
-  ┴│👋 Hey there, ${conn.getName(m.sender)}!
-  ⬡│🤖 I'm Ikratos-MD-v1 and I'm up and running! 🚀
-  │╰──────────────────────────
-  ┠──────[ BOT INFO ]───────╮
-  │╭──────────────────────────
-  ┴│ Ikratos-MD-v1
-  ⬡│💻 A WhatsApp bot created by:
-  ⬡│👨‍💻 Ahmad Ali 🇵🇰
-  ⬡│📞 For any inquiries, contact:
-  ⬡│📱 wa.me/923470027813
-  ⬡│🙅‍♂️ Please don't call the owner or you will be
-  ⬡│ ignored or blocked without warning! 😬
-  │╰──────────────────────────
-  ┠──────[ HOW TO USE ]───────⋆
-  ┴│💡 Here are some commands you can use:
-  ⬡│🔸 .menu - Shows a list of available commands
-  ⬡│🔸 .play - Plays a YouTube video or audio.
-  ⬡│🔸 .sticker - Converts an image to a sticker
-  ┬│🔸 .translate - Translates text to another language
-  │╰───────────────────
-  ╰───────────═┅═──────────
-  *Thank you for choosing Ikratos-MD-v1! 🌟✨*
+╭───[Ikratos-MD-v1]───╮
+│╭────────────────────
+┴│👋 Hey there, ${conn.getName(m.sender)}!
+⬡│🤖 I'm Ikratos-MD-v1\n⬡│ I'm up and running! 🚀
+│╰────────────────────
+┠───[ BOT INFO ]────╮
+│╭────────────────────
+┴│ Ikratos-MD-v1
+⬡│💻 A WhatsApp bot created by:
+⬡│👨‍💻 Ahmad Ali 🇵🇰
+⬡│📞 For any inquiries, contact:
+⬡│📱 wa.me/923470027813
+⬡│🙅‍♂️ Don't call the owner or you\'ll be
+⬡│ ignored or blocked! 😬
+│╰────────────────────
+┠───[ HOW TO USE ]────⋆
+┴│💡 Some commands you can use:
+⬡│🔸 .menu \n⬡│- Shows a list of available commands
+⬡│🔸 .play \n⬡│- Plays a YouTube video or audio.
+⬡│🔸 .sticker \n⬡│- Converts an image to a sticker
+┬│🔸 .translate \n⬡│- Translates text to other lang
+│╰──────────────
+╰────────═┅═───────
+*Thank you for choosing Ikratos-MD-v1! 🌟✨*
 `.trim()
   m.reply(caption)
 }
