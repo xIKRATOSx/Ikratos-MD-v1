b14bd27760832a5c71d2d9b4977b56292babc6f9
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-04-28 16:36:16 +0500

    v1.0.0

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,24 +1,27 @@
-const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 export async function before(m, { isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let chat = global.db.data.chats[m.chat]
-    let bot = global.db.data.settings[this.user.jid] || {}
-    const isGroupLink = linkRegex.exec(m.text)
-    let hapus = m.key.participant
-    let pesan = m.key.id
+if (m.isBaileys && m.fromMe)
+return !0
+if (!m.isGroup) return !1
+let chat = global.db.data.chats[m.chat]
+let delet = m.key.participant
+let bang = m.key.id
+let bot = global.db.data.settings[this.user.jid] || {}
+const isGroupLink = linkRegex.exec(m.text)
+const grupo = `https://chat.whatsapp.com`
+if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply('*Hey!! The antilink is active, but to save you honey you are admin😎*')
+if (chat.antiLink && isGroupLink && !isAdmin) {
+if (isBotAdmin) {
+const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+if (m.text.includes(linkThisGroup)) return !0
+}    
+await m.reply(`*「 ANTI LINKS 」*\n\n*Hasta la vista baby 👋, ${await this.getName(m.sender)} you broke the group rules you will be removed...!!*`)
 
-    if (chat.antiLink && isGroupLink && !isAdmin) {
-        if (isBotAdmin) {
-            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-            if (m.text.includes(linkThisGroup)) return !0
-        }
-        await conn.sendButton(m.chat, `*Group link detect!*${isBotAdmin ? '' : '\n\n_Bot not admin_  t_t'}`, author, ['off antilink', '/disable antilink'], m)
-        if (isBotAdmin && bot.restrict) {
-           return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: pesan, participant: hapus }})
-            return conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!bot.restrict) return m.reply('Owner & Admin Not Removed!')
-    }
-    return !0
+if (!isBotAdmin) return m.reply('*I\'ll save you baby, I\'m not an admin I can\'t remove you*')  
+if (isBotAdmin) {
+await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+} else if (!bot.restrict) return m.reply('*the owner of the bot does not have the restriction activated (enable restrict) contact him to enable them*')
+}
+return !0
 }
