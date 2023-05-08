f4726b81ff1a55a86bdf121953ebbe32edcbf8ce
Ahmad Ali <99126772+xIKRATOSx@users.noreply.github.com>
2023-05-08 18:05:33 +0500

    v1.0.0

--- plugins/_autoreact.js
+++ plugins/_autoreact.js
@@ -18,3 +18,13 @@ export default handler
      return list[Math.floor(Math.random() * list.length)]
   }
 */
+
+/*m.react = async (text) => {
+  // send a reaction to the message
+  await conn.sendMessage(m.chat, {
+    react: {
+      text: text,
+      key: m.key
+    }
+  })
+}*/
